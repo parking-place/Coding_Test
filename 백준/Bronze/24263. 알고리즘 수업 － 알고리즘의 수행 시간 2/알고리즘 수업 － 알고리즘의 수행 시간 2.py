c4428f6c8bfe783fecab836bbe44bcb07c6e4501
Parking-place <hyenzzang94@gmail.com>
2023-04-12 16:15:33 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
@@ -0,0 +1,13 @@
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         sum <- sum + A[i]; # 코드1
+#     return sum;
+# }
+
+n = int(input())
+
+# 코드1 시행 횟수
+print(n)
+# 최대차수
+print(1)
