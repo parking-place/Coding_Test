96e327bc5588e3409e4902b4f310f8770220687c
Parking-place <hyenzzang94@gmail.com>
2023-04-12 14:33:09 +0900

    [Bronze III] Title: 대지, Time: 2032 ms, Memory: 35196 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,14 +1,13 @@
 n = int(input())
 
 # 포인트 리스트
-point_l = []
+# point_l 를 굳이 만들지 않고 x_l, y_l를 만들어도 됨
+x_l, y_l = [], []
 for _ in range(n):
-    point_l.append(list(map(int, input().split())))
-
-# x, y 좌표를 각각 리스트로 만듬
-x_l = [ p[0] for p in point_l ]
-y_l = [ p[1] for p in point_l ]
-
+    x, y = list(map(int, input().split()))
+    x_l.append(x)
+    y_l.append(y)
+    
 # x, y 좌표중 최대, 최소값을 구함
 e, w, n, s = max(x_l), min(x_l), max(y_l), min(y_l)
 
