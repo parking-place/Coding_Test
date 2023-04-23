dd0e07e6c1923f4f16ea5eea04bf58c3582c9aef
Parking-place <hyenzzang94@gmail.com>
2023-04-23 23:15:42 +0900

    [level 2] Title: 피보나치 수, Time: 386.46 ms, Memory: 446 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,24 @@
+# 피보나치를 저장 할 딕셔너리
+fibonacci_dict = {
+    1 : 1,
+    2 : 1
+}
+
+def fibonacci(n) :
+    # 딕셔너리에서 먼저 확인
+    if n in fibonacci_dict :
+        return fibonacci_dict[n]         
+
+    # 없는경우에만 재귀를 이용해 구함
+    result = fibonacci(n-1) + fibonacci(n-2)
+    # 이미 구해진 값들을 참조하므로 성능상 이점이 큼
+    fibonacci_dict[n] = result
+    return result
+
+def solution(n):
+    # 3부터 n-1까지 피보나치 수를 구함
+    for i in range(3, n):
+        fibonacci(i)
+    # n번째 피보나치 수를 구함
+    answer = fibonacci(n)
+    return answer % 1234567
