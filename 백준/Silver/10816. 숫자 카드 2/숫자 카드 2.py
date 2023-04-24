40b7011215ed23924260b272cf8a828d2d15a800
Parking-place <hyenzzang94@gmail.com>
2023-04-24 12:05:23 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 972 ms, Memory: 240592 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -3,16 +3,13 @@ cards = list(map(int, input().split()))
 m= int(input())
 finds = list(map(int, input().split()))
 
-card_dict = {}
+# 카드의 이름별로 갯수를 저장할 리스트
+# 음수는 뒤쪽부터 저장됨
+card_list = [0] * 20000001
 # 카드의 이름별로 갯수를 저장
 for card in cards:
-    try:
-        card_dict[card] += 1
-    except:
-        card_dict[card] = 1
+    card_list[card] += 1
+    
 # 찾는 카드의 갯수를 출력
 for card in finds:
-    try:
-        print(card_dict[card], end=' ')
-    except:
-        print(0, end=' ')
+    print(card_list[card], end=' ')
