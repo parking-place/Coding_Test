b7e6101f030e0d24d1c04ce8178acde8e9c8edde
Parking-place <hyenzzang94@gmail.com>
2023-04-25 16:55:45 +0900

    [level 2] Title: 전화번호 목록, Time: 65.28 ms, Memory: 29.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -10,10 +10,8 @@ def solution(phone_book):
     for phone in phone_book:
         # 가장 짧은 번호의 길이 ~ 번호 길이까지 반복
         for i in range(min_len, len(phone)+1):
-            # print(f'check {phone} in {phone[:i]}')
             # 번호 길이가 끝까지 반복했을 경우
             if i == len(phone):
-                # print(f'add {phone} to dict')
                 # 중복된 번호는 없으므로 키를 생성
                 phone_dict[phone] = False
                 continue
