d293053fd4507758aee19a7522a707508a13e52c
Parking-place <hyenzzang94@gmail.com>
2023-04-25 12:11:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 59.22 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,15 @@
 def solution(participant, completion):
-    # 이름순 정렬
-    participant.sort()
-    completion.sort()
-    # 같은 번호끼리 비교시
-    # 이름이 달라질 경우
-    #  participant에 있는 이름이 완주 못함
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    return participant[-1]
+    # 딕셔너리 생성
+    dict_ = {}
+    # 이름 key, value = 총 명수
+    for name in participant:
+        dict_[name] = dict_.get(name, 0) + 1
+    # 완주한 사람은 -1 -> 0이 되면 삭제
+    for name in completion:
+        dict_[name] -= 1
+        if dict_[name] == 0:
+            del dict_[name]
+    # 남은 이름이 완주 못한 사람
+    answer = list(dict_.keys())[0]
+    
+    return answer
