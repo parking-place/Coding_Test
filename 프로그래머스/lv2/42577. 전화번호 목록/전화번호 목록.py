2a8c3629055bd6297d6ac98f4a320bbfacd2ec5a
Parking-place <hyenzzang94@gmail.com>
2023-04-25 16:54:14 +0900

    [level 2] Title: 전화번호 목록, Time: 63.48 ms, Memory: 29.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -4,10 +4,12 @@ def solution(phone_book):
     phone_dict = {}
     # 길이 순 정렬 -> 짧은 전화번호부터 딕셔너리에 저장 위함
     phone_book.sort(key=lambda x: len(x))
+    # 가장 짧은 번호의 길이
+    min_len = len(phone_book[0])
     # 전화번호부의 전화번호를 하나씩 처리
     for phone in phone_book:
-        # 1 ~ 번호 길이까지 반복
-        for i in range(1, len(phone)+1):
+        # 가장 짧은 번호의 길이 ~ 번호 길이까지 반복
+        for i in range(min_len, len(phone)+1):
             # print(f'check {phone} in {phone[:i]}')
             # 번호 길이가 끝까지 반복했을 경우
             if i == len(phone):
@@ -18,8 +20,7 @@ def solution(phone_book):
             # 번호를 i개만큼 자른 것이 딕셔너리에 있을 경우
             try:
                 # 접두어인 번호가 있으므로 False반환
-                check = phone_dict[phone[:i]]
-                return False
+                return phone_dict[phone[:i]]
             except:
                 # 접두어인 번호가 없으므로 다음 번호로 넘어감
                 continue
